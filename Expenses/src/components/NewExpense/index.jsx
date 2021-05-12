0cd2b559e9ea2c9f74ec08443080256bf5e99282
João Pedro <pedrobarros2010@gmail.com>
2021-05-12 15:59:33 -0300

    feat: add button to show or not new expense form

--- Expenses/src/components/NewExpense/index.jsx
+++ Expenses/src/components/NewExpense/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseForm from '../ExpenseForm'
 
 import './styles.css'
 
 const NewExpense = (props) => {
+	const [showForm, setShowForm] = useState(false)
 	const saveExpenseDataHandler = (enteredExpenseData) => {
 		const expenseData = {
 			...enteredExpenseData,
@@ -13,9 +14,20 @@ const NewExpense = (props) => {
 		props.onAddExpense(expenseData)
 	}
 
+	const showFormHandler = () => {
+		setShowForm((prevState) => !prevState)
+	}
+
 	return (
 		<div className='new-expense'>
-			<ExpenseForm onNewExpense={saveExpenseDataHandler} />
+			{showForm ? (
+				<ExpenseForm
+					onNewExpense={saveExpenseDataHandler}
+					onCancelNewExpense={showFormHandler}
+				/>
+			) : (
+				<button onClick={showFormHandler}>Add new expense.</button>
+			)}
 		</div>
 	)
 }
