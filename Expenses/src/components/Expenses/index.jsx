0d3374f165ea1ceed7f9ff8c710f7e2c8d2c851a
João Pedro <pedrobarros2010@gmail.com>
2021-05-12 11:42:54 -0300

    feat: create expense filter component

--- Expenses/src/components/Expenses/index.jsx
+++ Expenses/src/components/Expenses/index.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../Card'
 import ExpenseItem from '../ExpenseItem'
+import ExpensesFilter from '../ExpensesFilter'
 
 import './styles.css'
 
 function Expenses(props) {
+	const [filteredYear, setFilteredYear] = useState('2020')
+
+	const yearFilterChangeHandler = (year) => {
+		setFilteredYear(year)
+	}
+
 	return (
 		<Card className='expenses'>
+			<ExpensesFilter
+				selected={filteredYear}
+				onChangeFilter={yearFilterChangeHandler}
+			/>
 			{props.items.map((expense, index) => (
 				<ExpenseItem
 					key={index}
