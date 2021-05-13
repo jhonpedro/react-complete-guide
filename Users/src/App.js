10426762d90e8171ae9328af74d93d6dd7ee3058
João Pedro <pedrobarros2010@gmail.com>
2021-05-13 16:22:16 -0300

    feat: add list users

--- Users/src/App.js
+++ Users/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewUser from './components/User/NewUser';
 import GlobalStyle from './assets/global';
+import ListUser from './components/User/ListUser';
 
 function App() {
+  const [users, setUsers] = useState([
+    {
+      name: 'João',
+      age: '20',
+      id: Math.random(),
+    },
+  ]);
+
+  function addUser(user) {
+    setUsers((prevState) => [user, ...prevState]);
+  }
+
   return (
     <>
-      <NewUser />
+      <NewUser onAddUser={addUser} />
+      <ListUser users={users} />
       <GlobalStyle />
     </>
   );
