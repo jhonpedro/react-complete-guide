e95af8cf6f64fa38e7b5241e8453a58f5015be09
João Pedro <pedrobarros2010@gmail.com>
2021-05-14 10:32:32 -0300

    feat: creating modal state

--- Users/src/App.js
+++ Users/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import NewUser from './components/User/NewUser';
 import GlobalStyle from './assets/global';
 import ListUser from './components/User/ListUser';
+import Modal from './components/Utils/Modal';
 
 function App() {
   const [users, setUsers] = useState([
@@ -12,15 +13,26 @@ function App() {
       id: Math.random(),
     },
   ]);
+  const [modal, setModal] = useState({
+    text: '',
+    isUp: false,
+  });
 
   function addUser(user) {
     setUsers((prevState) => [user, ...prevState]);
   }
 
+  function isUpHandler(text) {
+    setModal((prevState) => {
+      return { text, isUp: !prevState.isUp };
+    });
+  }
+
   return (
     <>
-      <NewUser onAddUser={addUser} />
+      <NewUser onAddUser={addUser} isUpHandler={isUpHandler} />
       <ListUser users={users} />
+      <Modal text={modal.text} isUp={modal.isUp} isUpHandler={isUpHandler} />
       <GlobalStyle />
     </>
   );
