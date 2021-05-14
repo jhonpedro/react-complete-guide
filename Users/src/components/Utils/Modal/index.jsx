0b15d50a1bcfc4e7ba29966f3047cf2af6290850
João Pedro <pedrobarros2010@gmail.com>
2021-05-14 14:33:02 -0300

    feat: creating portals

--- Users/src/components/Utils/Modal/index.jsx
+++ Users/src/components/Utils/Modal/index.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Button from '../../UI/Button';
 import { ModalBox, ModalContainer } from './styles';
+
+const ModalElement = ({ text, clickHandler }) => (
+  <ModalContainer>
+    <ModalBox>
+      <p>{text}</p>
+      <div>
+        <Button onClick={clickHandler}>Ok</Button>
+      </div>
+    </ModalBox>
+  </ModalContainer>
+);
 
 const Modal = ({ text, isUp, isUpHandler }) => {
   function clickHandler() {
     isUpHandler();
   }
 
-  return !isUp ? null : (
-    <ModalContainer>
-      <ModalBox>
-        <p>{text}</p>
-        <div>
-          <Button onClick={clickHandler}>Ok</Button>
-        </div>
-      </ModalBox>
-    </ModalContainer>
-  );
+  return !isUp
+    ? null
+    : ReactDOM.createPortal(
+        <ModalElement text={text} clickHandler={clickHandler} />,
+        document.getElementById('modal')
+      );
 };
 
 export default Modal;
