1e84ed66a531162f75a6f05f48892e06dd484cfe
João Pedro <pedrobarros2010@gmail.com>
2021-05-17 12:05:10 -0300

    feat: create a provider

--- Login/src/store/auth-context.js
+++ Login/src/store/auth-context.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AuthContext = React.createContext({ isLoggedIn: false });
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
