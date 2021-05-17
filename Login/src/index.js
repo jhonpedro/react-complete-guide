463e8aad82b4aa80812b0a3d66b9d6a832f759e4
João Pedro <pedrobarros2010@gmail.com>
2021-05-17 12:06:16 -0300

    feat: apply auth provider

--- Login/src/index.js
+++ Login/src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import { AuthProvider } from './store/auth-context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
+  document.getElementById('root')
+);
