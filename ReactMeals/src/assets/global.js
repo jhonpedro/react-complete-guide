10daad9f3e5d69cd9fb69adaadc5577f7b26cb00
João Pedro <pedrobarros2010@gmail.com>
2021-05-17 16:10:54 -0300

    feat: add base colors

--- ReactMeals/src/assets/global.js
+++ ReactMeals/src/assets/global.js
@@ -1,6 +1,15 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  :root {
+    font-size: 62.5%;
+
+    --color-pink-lighter: #faf3dd;
+    --color-primary: #aed9e0;
+    --color-secondary: #b8f2e6;
+    --color-dark: #5e6472;
+  }
+
   * {
     padding: 0;
     margin: 0;
