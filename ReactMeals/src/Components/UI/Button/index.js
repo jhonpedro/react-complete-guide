1a1da1ee7db2eeb19da8304789228031f380b3ea
João Pedro <pedrobarros2010@gmail.com>
2021-05-18 12:01:17 -0300

    feazt: create button styled component

---
+++ ReactMeals/src/Components/UI/Button/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  border-radius: 1rem;
+  font-weight: bold;
+  font-size: 1.7rem;
+  border: none;
+`
+
+export default Button
