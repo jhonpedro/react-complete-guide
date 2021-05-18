6c60d48a95bea427a423a52aa69c4e9cab16cb59
João Pedro <pedrobarros2010@gmail.com>
2021-05-18 16:30:58 -0300

    feat: create bump and show modal on click

--- ReactMeals/src/Components/Cart/CartButton/index.jsx
+++ ReactMeals/src/Components/Cart/CartButton/index.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FiShoppingCart } from 'react-icons/fi'
+import { useStore } from '../../../store/context'
 
 import { CartButtonContainer, CartButtonCount } from './styles'
 
 const CartButton = () => {
+  const { handleClickIsModalShowing, getCartItems } = useStore()
+  const [bumb, setBumb] = useState(false)
+  const cartItemsLength = getCartItems().length
+
+  useEffect(() => {
+    return () => {
+      setBumb(true)
+      setTimeout(() => setBumb(false), 500)
+    }
+  }, [cartItemsLength])
+
+  const showModal = () => {
+    handleClickIsModalShowing()
+  }
+
   return (
-    <CartButtonContainer>
+    <CartButtonContainer onClick={showModal}>
       <FiShoppingCart />
       <span>Your Cart</span>
-      <CartButtonCount>0</CartButtonCount>
+      <CartButtonCount bump={bumb}>
+        <span>{cartItemsLength}</span>
+      </CartButtonCount>
     </CartButtonContainer>
   )
 }
--- ReactMeals/src/Components/Cart/CartButton/styles.js
+++ ReactMeals/src/Components/Cart/CartButton/styles.js
@@ -1,9 +1,11 @@
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 
 export const CartButtonContainer = styled.div`
   background-color: var(--color-primary);
   border-radius: 3rem;
   padding: 1.5rem 7rem;
+  cursor: pointer;
+  overflow: hidden;
 
   display: flex;
   justify-content: center;
@@ -21,18 +23,45 @@ export const CartButtonContainer = styled.div`
   }
 `
 
-export const CartButtonCount = styled.strong`
-  height: 3.5rem;
-  width: 3.5rem;
+const fromUp = keyframes`
+  0% {
+    top: -2rem;
+  }
+  100% {
+    top: 0.8rem;
+  }
+`
 
+export const CartButtonCount = styled.strong`
   display: flex;
   justify-content: center;
   align-items: center;
+  overflow: hidden;
+  position: relative;
 
   font-size: 2rem;
 
-  padding: 1rem;
+  height: 3.5rem;
+  width: 3.5rem;
+
+  padding: 0.5rem;
   font-weight: bold;
-  border-radius: 2rem;
+  border-radius: 4rem;
   background-color: var(--color-secondary);
+
+  span {
+    position: relative;
+    overflow: hidden;
+  }
+
+  ${(props) => {
+    return props.bump
+      ? css`
+          span {
+            position: absolute;
+            animation: ease ${fromUp} 500ms;
+          }
+        `
+      : ''
+  }}
 `
