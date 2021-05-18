22ebb487a4ec0c9c84b016b3d7828d43c4b16ae0
João Pedro <pedrobarros2010@gmail.com>
2021-05-18 14:10:36 -0300

    feat: format price

--- ReactMeals/src/Components/Food/FoodItems/index.jsx
+++ ReactMeals/src/Components/Food/FoodItems/index.jsx
@@ -3,12 +3,18 @@ import Button from '../../UI/Button'
 import { AddToCartContainer, FoodInfo, FoodItemsContainer } from './styles'
 
 const FoodItems = ({ name, description, price }) => {
+  const formatedPrice = price
+    .toString()
+    .replace(/(\d+)(.\d+)?/, (_, n1, n2) => {
+      return `${n1},${!n2 ? '00' : n2}`
+    })
+
   return (
     <FoodItemsContainer>
       <FoodInfo>
         <strong>{name}</strong>
         <p>{description}</p>
-        <span>{price}</span>
+        <span>R${formatedPrice}</span>
       </FoodInfo>
       <AddToCartContainer>
         <label>
