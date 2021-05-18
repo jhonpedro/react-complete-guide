10103c2190ae8ac42155c3857f915537d169abc5
João Pedro <pedrobarros2010@gmail.com>
2021-05-18 16:32:29 -0300

    feat: add store provider and modal

--- ReactMeals/src/index.js
+++ ReactMeals/src/index.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import Modal from './Components/Cart/Modal'
+import { StoreProvider } from './store/context'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <StoreProvider>
+    <App />
+    <Modal />
+  </StoreProvider>,
+  document.getElementById('root')
+)
