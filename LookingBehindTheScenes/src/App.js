612321ad99f0f1c6ea23538ea94121b45a0a1399
João Pedro <pedrobarros2010@gmail.com>
2021-05-19 15:17:06 -0300

    base looking behind the scenes app

---
+++ LookingBehindTheScenes/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
+import Button from './components/UI/Button/Button';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const handleClickShowParagraph = () =>
+    setShowParagraph((prevState) => !prevState);
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={handleClickShowParagraph}>Toogle paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ LookingBehindTheScenes/src/components/Demo/DemoOutput.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DemoOutput = ({ show }) => {
+  return <p>{show ? 'This is new!' : ''}</p>;
+};
+
+export default DemoOutput;
