fda05ec13afb39a3c90adc23ba0305eabe67ef6e
João Pedro <pedrobarros2010@gmail.com>
2021-05-19 10:18:16 -0300

    feat: edit cart quantity of an item based on index given

--- ReactMeals/src/Components/Cart/CartItem/index.jsx
+++ ReactMeals/src/Components/Cart/CartItem/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useStore } from '../../../store/context'
 import formatPrice from '../../../utils/formatPrice'
 import { AddToQuantity, CartItemContainer, CartItemInfo } from './styles'
 
-const CartItem = ({ name, price, quantity }) => {
+const CartItem = ({ name, price, quantity, index }) => {
+  const { editCartItemQuantity } = useStore()
   const formatedPrice = formatPrice(price)
+
+  const handleSubtractionItem = () => {
+    editCartItemQuantity(index, 'sub')
+  }
+
+  const handleSumItem = () => {
+    editCartItemQuantity(index, 'sum')
+  }
 
   return (
     <CartItemContainer>
@@ -15,8 +25,8 @@ const CartItem = ({ name, price, quantity }) => {
         </div>
       </CartItemInfo>
       <AddToQuantity>
-        <button>-</button>
-        <button>+</button>
+        <button onClick={handleSubtractionItem}>-</button>
+        <button onClick={handleSumItem}>+</button>
       </AddToQuantity>
     </CartItemContainer>
   )
