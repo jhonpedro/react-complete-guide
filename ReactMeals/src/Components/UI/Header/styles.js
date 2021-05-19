b035e86acb79e302583205d8c12722851a8c055b
João Pedro <pedrobarros2010@gmail.com>
2021-05-19 09:41:16 -0300

    fix: change position

--- ReactMeals/src/Components/UI/Header/styles.js
+++ ReactMeals/src/Components/UI/Header/styles.js
@@ -2,11 +2,15 @@ import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
   height: 8rem;
+  width: 100%;
+
   background-color: var(--color-secondary);
   display: flex;
   justify-content: space-between;
   align-items: center;
   font-weight: bold;
+  position: fixed;
+  z-index: 12;
 
   padding: 0 8rem;
 
