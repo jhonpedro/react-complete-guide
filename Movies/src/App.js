fcc1df91454725ab68877b3e77d09bc8357b30fe
João Pedro <pedrobarros2010@gmail.com>
2021-05-20 15:44:46 -0300

    refactor: moving to async/await code

--- Movies/src/App.js
+++ Movies/src/App.js
@@ -6,23 +6,19 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const getMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films')
-      .then((response) => {
-        return response.json();
+  const getMoviesHandler = async () => {
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+    setMovies(
+      data.results.map((movieItem) => {
+        return {
+          id: movieItem.episode_id,
+          title: movieItem.title,
+          openingText: movieItem.opening_crawl,
+          releaseDate: movieItem.release_date,
+        };
       })
-      .then((data) => {
-        setMovies(
-          data.results.map((movieItem) => {
-            return {
-              id: movieItem.episode_id,
-              title: movieItem.title,
-              openingText: movieItem.opening_crawl,
-              releaseDate: movieItem.release_date,
-            };
-          })
-        );
-      });
+    );
   };
 
   return (
