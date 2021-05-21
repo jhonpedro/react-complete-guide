33a22caa83e52fa9e3bdbed8f0aaa05ce4ac4bff
João Pedro <pedrobarros2010@gmail.com>
2021-05-21 11:32:20 -0300

    fix: set dependencies in get operations result and in use effect

--- CustomHooks/src/hooks/useCounter.js
+++ CustomHooks/src/hooks/useCounter.js
@@ -1,18 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const useCounter = (operation) => {
   const [counter, setCounter] = useState(0);
 
-  const getOperationResult = (n1, n2) => {
-    switch (operation) {
-      case '+':
-        return n1 + n2;
-      case '-':
-        return n1 - n2;
-      default:
-        throw new Error('Should provide an operation in useCounter');
-    }
-  };
+  const getOperationResult = useCallback(
+    (n1, n2) => {
+      switch (operation) {
+        case '+':
+          return n1 + n2;
+        case '-':
+          return n1 - n2;
+        default:
+          throw new Error('Should provide an operation in useCounter');
+      }
+    },
+    [operation]
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -20,7 +23,7 @@ const useCounter = (operation) => {
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [getOperationResult]);
 
   return counter;
 };
