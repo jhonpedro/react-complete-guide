457e8e40a24aa1d9f8a8012f818e9d30ff7757f1
João Pedro <pedrobarros2010@gmail.com>
2021-05-24 14:37:49 -0300

    fix: remove dot from group and escape it

--- ReactMeals/src/utils/formatPrice.js
+++ ReactMeals/src/utils/formatPrice.js
@@ -1,5 +1,5 @@
 export default function formatPrice(price) {
-  return price.toString().replace(/(\d+)(.\d+)?/, (_, n1, n2) => {
+  return price.toString().replace(/(\d+)\.(\d+)?/, (_, n1, n2) => {
     return `${n1},${!n2 ? '00' : n2}`
   })
 }
