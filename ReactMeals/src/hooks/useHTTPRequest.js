6692ba12122787b0c9bc224a1ef6383510acde48
João Pedro <pedrobarros2010@gmail.com>
2021-05-24 14:38:15 -0300

    feat: create http request hook

---
+++ ReactMeals/src/hooks/useHTTPRequest.js
@@ -0,0 +1,37 @@
+import { useCallback, useState } from 'react'
+
+const useHTTPRequest = (url, method, headers) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const request = useCallback(
+    async (body) => {
+      setIsLoading(true)
+      setError(null)
+      try {
+        const response = await fetch(url, {
+          method: !method ? 'GET' : method,
+          body: !body ? null : body,
+          headers: !headers ? {} : headers,
+        })
+
+        if (!response.ok) {
+          throw new Error('Request failed!')
+        }
+
+        const data = await response.json()
+
+        return data
+      } catch (err) {
+        setError(err.message || 'Something went wrong!')
+      } finally {
+        setIsLoading(false)
+      }
+    },
+    [url, method, headers]
+  )
+
+  return [isLoading, error, request]
+}
+
+export default useHTTPRequest
