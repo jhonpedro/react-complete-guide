d2686d0b5b2db877567143272e7e5eeebc9d3b79
João Pedro <pedrobarros2010@gmail.com>
2021-05-24 13:55:14 -0300

    feat: challenge with other form

--- FormsAndUserInput/src/components/BasicForm.js
+++ FormsAndUserInput/src/components/BasicForm.js
@@ -1,22 +1,102 @@
+import React from 'react';
+import useInput from '../hooks/userInput';
+
 const BasicForm = (props) => {
+  const {
+    value: firstNameInput,
+    hasErrors: firstNameWasTouchedAndIsInvalid,
+    inputBlurHandler: firstNameBlurHandler,
+    onChangeValueHandler: firstNameOnChangeHandler,
+    reset: resetFirstName,
+  } = useInput((value) => value.trim().length >= 3);
+  const {
+    value: secondNameInput,
+    hasErrors: secondNameWasTouchedAndIsInvalid,
+    inputBlurHandler: secondNameBlurHandler,
+    onChangeValueHandler: secondNameOnChangeHandler,
+    reset: resetSecondName,
+  } = useInput((value) => value.trim().length >= 3);
+  const {
+    value: emailInput,
+    isValid: emailIsValid,
+    hasErrors: emailWasTouchedAndIsInvalid,
+    inputBlurHandler: emailBlurHandler,
+    onChangeValueHandler: emailOnChangeHandler,
+    reset: resetEmail,
+    wasTouched: emailInputWasTouched,
+  } = useInput((value) => value.includes('@'));
+
+  const formIsValid =
+    !firstNameWasTouchedAndIsInvalid &&
+    !secondNameWasTouchedAndIsInvalid &&
+    !emailWasTouchedAndIsInvalid;
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    resetFirstName();
+    resetSecondName();
+    resetEmail();
+  };
+
+  const firstNameClass = firstNameWasTouchedAndIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+  const secondNameClass = secondNameWasTouchedAndIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+  const emailClass = emailWasTouchedAndIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={firstNameClass}>
           <label htmlFor="firstName">First Name</label>
-          <input type="text" id="firstName" />
+          <input
+            type="text"
+            id="firstName"
+            value={firstNameInput}
+            onBlur={firstNameBlurHandler}
+            onChange={firstNameOnChangeHandler}
+          />
+          {firstNameWasTouchedAndIsInvalid && (
+            <p className="error-text">
+              First name must have at least three letters
+            </p>
+          )}
         </div>
-        <div className="form-control">
+        <div className={secondNameClass}>
           <label htmlFor="secondName">Last Name</label>
-          <input type="text" id="secondName" />
+          <input
+            type="text"
+            id="secondName"
+            value={secondNameInput}
+            onBlur={secondNameBlurHandler}
+            onChange={secondNameOnChangeHandler}
+          />
+          {secondNameWasTouchedAndIsInvalid && (
+            <p className="error-text">
+              Second name must have at least three letters
+            </p>
+          )}
         </div>
       </div>
-      <div className="form-control">
+      <div className={emailClass}>
         <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          value={emailInput}
+          onBlur={emailBlurHandler}
+          onChange={emailOnChangeHandler}
+        />
+        {!emailIsValid && emailInputWasTouched && (
+          <p className="error-text">Name must have at least three letters</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
