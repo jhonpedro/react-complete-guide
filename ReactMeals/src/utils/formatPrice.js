d0e3e4ea8e4845598e564dff4aee5c2035b7a41d
João Pedro <pedrobarros2010@gmail.com>
2021-05-24 16:01:58 -0300

    fix: change n2 conditional

--- ReactMeals/src/utils/formatPrice.js
+++ ReactMeals/src/utils/formatPrice.js
@@ -1,5 +1,6 @@
 export default function formatPrice(price) {
   return price.toString().replace(/(\d+)\.(\d+)?/, (_, n1, n2) => {
-    return `${n1},${!n2 ? '00' : n2}`
+    n2 = !n2 ? '00' : n2
+    return `${n1},${n2}`
   })
 }
