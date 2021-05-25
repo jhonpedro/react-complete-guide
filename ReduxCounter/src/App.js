dee7b306e8b3b6c085c8ddc30b98bbe5cb23e15d
João Pedro <pedrobarros2010@gmail.com>
2021-05-25 15:52:51 -0300

    feat: implement auth

--- ReduxCounter/src/App.js
+++ ReduxCounter/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
   return (
     <React.Fragment>
       <Header />
-      <Auth />
+      {!isAuthenticated && <Auth />}
       <Counter />
     </React.Fragment>
   );
--- ReduxCounter/src/components/Auth.jsx
+++ ReduxCounter/src/components/Auth.jsx
@@ -1,19 +1,49 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store/slices/authSlice';
+
 import classes from './Auth.module.css';
 
 const Auth = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [errors, setErrors] = useState('');
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setErrors('');
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [errors]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!emailRef.current.value || !passwordRef.current.value) {
+      setErrors('Verify your credentials');
+      return;
+    }
+
+    dispatch(authActions.login());
+  };
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={submitHandler}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" ref={emailRef} />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" ref={passwordRef} />
           </div>
-          <button>Login</button>
+          {errors && <p>{errors}</p>}
+          <button type="submit">Login</button>
         </form>
       </section>
     </main>
