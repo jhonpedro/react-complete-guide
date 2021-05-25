18aa02ce2ca884366703ede8ea38c0a6bc8d7606
João Pedro <pedrobarros2010@gmail.com>
2021-05-25 15:59:54 -0300

    fix: show user profile

--- ReduxCounter/src/App.js
+++ ReduxCounter/src/App.js
@@ -3,6 +3,7 @@ import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
 import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
 
 function App() {
   const { isAuthenticated } = useSelector((state) => state.auth);
@@ -11,6 +12,7 @@ function App() {
     <React.Fragment>
       <Header />
       {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
       <Counter />
     </React.Fragment>
   );
