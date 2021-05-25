965a806282cce915b82c2ebb4a3a3bf12d2a03b0
João Pedro <pedrobarros2010@gmail.com>
2021-05-25 11:57:03 -0300

    feat: create toggle counter and increase by functionality

--- ReduxCounter/src/components/Counter.jsx
+++ ReduxCounter/src/components/Counter.jsx
@@ -2,22 +2,30 @@ import classes from './Counter.module.css';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { Component } from 'react';
 
+const INCREMENT_BY = 3;
+
 const Counter = () => {
-  const counter = useSelector((state) => state.counter);
+  const { counter, showCounter } = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'TOGGLE_COUNTER' });
+  };
 
   const incrementHandler = () => dispatch({ type: 'INCREMENT' });
 
   const decrementHandler = () => dispatch({ type: 'DECREMENT' });
 
+  const increaseBy = () =>
+    dispatch({ type: 'INCREMENT_BY', payload: INCREMENT_BY });
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseBy}>Increment by {INCREMENT_BY}</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- ReduxCounter/src/store/index.js
+++ ReduxCounter/src/store/index.js
@@ -2,15 +2,22 @@ import { createStore } from 'redux';
 
 const INITIAL_VAlUE = {
   counter: 0,
+  showCounter: true,
 };
 
 const mainReducer = (state = INITIAL_VAlUE, action) => {
   switch (action.type) {
     case 'INCREMENT': {
-      return { counter: state.counter + 1 };
+      return { ...state, counter: state.counter + 1 };
     }
     case 'DECREMENT': {
-      return { counter: state.counter - 1 };
+      return { ...state, counter: state.counter - 1 };
+    }
+    case 'INCREMENT_BY': {
+      return { ...state, counter: state.counter + action.payload };
+    }
+    case 'TOGGLE_COUNTER': {
+      return { ...state, showCounter: !state.showCounter };
     }
     default: {
       return { ...state };
