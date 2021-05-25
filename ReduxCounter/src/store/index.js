9aee39b5d80811c9b8b1f2d3894e9ea1862d777a
João Pedro <pedrobarros2010@gmail.com>
2021-05-25 15:52:39 -0300

    feat: add auth slice to reducers

--- ReduxCounter/src/store/index.js
+++ ReduxCounter/src/store/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authSlice from './slices/authSlice';
 import counterSlice from './slices/counterSlice';
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer },
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export default store;
