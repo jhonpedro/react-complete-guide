102142809686586257f6c4c245c1f1003676e690
João Pedro <pedrobarros2010@gmail.com>
2021-05-26 15:53:50 -0300

    feat: create replace, send cart data and get cart data actions

--- ReduxAdvanced/src/store/reducers/cart/actions.js
+++ ReduxAdvanced/src/store/reducers/cart/actions.js
@@ -1,8 +1,13 @@
-import { ADD_TO_CART, CHANGE_QUANTITY } from './actionTypes';
+import { actionNotification } from '../ui/actions';
+import { ADD_TO_CART, CHANGE_QUANTITY, REPLACE_CART } from './actionTypes';
 
 export const actionAddToCart = (cartItem) => ({
   type: ADD_TO_CART,
   payload: cartItem,
+});
+export const actionReplaceCart = (newCart) => ({
+  type: REPLACE_CART,
+  payload: newCart,
 });
 
 export const actionChangeCartQuantity = (cartItemTitle, quantityAction) => ({
@@ -12,3 +17,62 @@ export const actionChangeCartQuantity = (cartItemTitle, quantityAction) => ({
     action: quantityAction,
   },
 });
+
+export const actionSendCartData = (cartItems) => {
+  return (dispatch) => {
+    const request = async () => {
+      const response = await fetch(
+        'https://test-f860d-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cartItems),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      dispatch(
+        actionNotification({
+          title: 'Success',
+          message: 'Everithing went Ok!',
+          status: 'success',
+        })
+      );
+    };
+
+    request().catch(() => {
+      dispatch(
+        actionNotification({
+          title: 'Error',
+          message: 'There was an error in the request',
+          status: 'error',
+        })
+      );
+    });
+  };
+};
+
+export const actionGetCart = () => {
+  return (dispatch) => {
+    const request = async () => {
+      const response = await fetch(
+        'https://test-f860d-default-rtdb.firebaseio.com/cart.json'
+      );
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const data = await response.json();
+
+      dispatch(actionReplaceCart(data));
+    };
+
+    request();
+  };
+};
