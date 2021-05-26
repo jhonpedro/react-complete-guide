8d536413d19911f66bbf8cf3b36aad2a30792b4d
João Pedro <pedrobarros2010@gmail.com>
2021-05-26 11:03:02 -0300

    feat: get cart items from store

--- ReduxAdvanced/src/components/Cart/Cart.js
+++ ReduxAdvanced/src/components/Cart/Cart.js
@@ -1,15 +1,31 @@
+import { useSelector } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
 const Cart = (props) => {
+  const cartItems = useSelector((state) => state.cart);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cartItems.map((cartItem) => {
+          const total =
+            parseInt(cartItem.quantity) * parseFloat(cartItem.price);
+
+          return (
+            <CartItem
+              key={cartItem.title}
+              item={{
+                title: cartItem.title,
+                quantity: cartItem.quantity,
+                total,
+                price: cartItem.price,
+              }}
+            />
+          );
+        })}
       </ul>
     </Card>
   );
