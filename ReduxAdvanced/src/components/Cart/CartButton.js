1897ff66694ce943510d83d73960816589c7106d
João Pedro <pedrobarros2010@gmail.com>
2021-05-26 11:04:40 -0300

    feat: change cart length dinamically

--- ReduxAdvanced/src/components/Cart/CartButton.js
+++ ReduxAdvanced/src/components/Cart/CartButton.js
@@ -1,10 +1,13 @@
 import classes from './CartButton.module.css';
+import { useSelector } from 'react-redux';
 
 const CartButton = (props) => {
+  const cartLength = useSelector((state) => state.cart.length);
+
   return (
     <button className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartLength}</span>
     </button>
   );
 };
