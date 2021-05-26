eeae5e6a3ffcbc4cb232434752474116fca9cc11
João Pedro <pedrobarros2010@gmail.com>
2021-05-26 11:25:17 -0300

    feat: add toggle cart functionality

--- ReduxAdvanced/src/components/Cart/CartButton.js
+++ ReduxAdvanced/src/components/Cart/CartButton.js
@@ -1,11 +1,17 @@
 import classes from './CartButton.module.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { actionToggleCart } from '../../store/reducers/ui/actions';
 
 const CartButton = (props) => {
   const cartLength = useSelector((state) => state.cart.length);
+  const dispatch = useDispatch();
+
+  const toggleCart = () => {
+    dispatch(actionToggleCart());
+  };
 
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCart}>
       <span>My Cart</span>
       <span className={classes.badge}>{cartLength}</span>
     </button>
