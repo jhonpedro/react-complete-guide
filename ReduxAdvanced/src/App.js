6c0979fc6cebe0a25cd998b8c3f7491de569384f
João Pedro <pedrobarros2010@gmail.com>
2021-05-26 15:58:25 -0300

    feat: apply actions of fetching and sending data

--- ReduxAdvanced/src/App.js
+++ ReduxAdvanced/src/App.js
@@ -1,16 +1,48 @@
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Notification from './components/UI/Notification';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import {
+  actionGetCart,
+  actionSendCartData,
+} from './store/reducers/cart/actions';
+
+let initial = true;
 
 function App() {
-  const showCart = useSelector((state) => state.ui.showCart);
+  const { showCart, notification } = useSelector((state) => state.ui);
+  const cartItems = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    actionGetCart()(dispatch);
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (initial) {
+      initial = false;
+      return;
+    }
+    actionSendCartData(cartItems)(dispatch);
+  }, [cartItems, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <React.Fragment>
+      {notification && (
+        <Notification
+          title={notification.title}
+          message={notification.message}
+          status={notification.status}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </React.Fragment>
   );
 }
 
