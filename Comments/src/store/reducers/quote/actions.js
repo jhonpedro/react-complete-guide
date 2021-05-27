80a74e8fb95e897e7966d8380e2aa16fe3855ec2
João Pedro <pedrobarros2010@gmail.com>
2021-05-27 15:18:27 -0300

    feat: create action add quote comment

--- Comments/src/store/reducers/quote/actions.js
+++ Comments/src/store/reducers/quote/actions.js
@@ -1,8 +1,15 @@
-import { ADD_QUOTE } from './actionTypes';
+import { ADD_QUOTE, ADD_QUOTE_COMMENT } from './actionTypes';
 
 export const actionAddQuote = (newQuote) => {
   return {
     type: ADD_QUOTE,
     payload: newQuote,
+  };
+};
+
+export const actionAddQuoteComment = (commentInfo) => {
+  return {
+    type: ADD_QUOTE_COMMENT,
+    payload: { comment: commentInfo.comment, quoteId: commentInfo.quoteId },
   };
 };
