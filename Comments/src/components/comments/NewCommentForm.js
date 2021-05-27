f1b3a4cede0ef537267b757a79e4b89b1119aca7
João Pedro <pedrobarros2010@gmail.com>
2021-05-27 15:20:16 -0300

    feat: dispatch action to add quote comment

--- Comments/src/components/comments/NewCommentForm.js
+++ Comments/src/components/comments/NewCommentForm.js
@@ -1,12 +1,23 @@
 import { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionAddQuoteComment } from '../../store/reducers/quote/actions';
+import generateRandomId from '../../utils/generateRandomId';
 
 import classes from './NewCommentForm.module.css';
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
+  const dispatch = useDispatch();
 
   const submitFormHandler = (event) => {
     event.preventDefault();
+
+    dispatch(
+      actionAddQuoteComment({
+        comment: { id: generateRandomId(), text: commentTextRef.current.value },
+        quoteId: props.quoteId,
+      })
+    );
 
     // optional: Could validate here
 
@@ -16,11 +27,11 @@ const NewCommentForm = (props) => {
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
