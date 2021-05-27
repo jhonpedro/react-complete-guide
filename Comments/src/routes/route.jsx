3a57ce579b3ee6f88e4f7dfd608a7e573b46f200
João Pedro <pedrobarros2010@gmail.com>
2021-05-27 15:10:02 -0300

    feat: add quote page route

--- Comments/src/routes/route.jsx
+++ Comments/src/routes/route.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import MainHeader from '../components/layout/MainNavigation';
 import AddTask from '../pages/AddQuote';
+import Quote from '../pages/Quote';
 import QuoteList from '../pages/QuoteList';
 
 const Routes = () => {
@@ -15,6 +16,9 @@ const Routes = () => {
           </Route>
           <Route path="/quotes" exact>
             <QuoteList />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <Quote />
           </Route>
           <Route path="*">
             <Redirect to="/new" />
