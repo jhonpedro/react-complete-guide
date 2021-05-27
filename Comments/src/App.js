8c153f51a24160b35b087d8ac5bab51029ac1135
João Pedro <pedrobarros2010@gmail.com>
2021-05-27 13:59:48 -0300

    fix: add browser router

--- Comments/src/App.js
+++ Comments/src/App.js
@@ -1,12 +1,15 @@
 import Routes from './routes/route';
 import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Routes />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </BrowserRouter>
   );
 }
 
