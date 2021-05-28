4cdd07967798729ec72881dfc71c0ac31d3b9ead
João Pedro <pedrobarros2010@gmail.com>
2021-05-28 15:58:15 -0300

    feat: pass props to profile form

--- LoginAuth/src/components/Profile/UserProfile.js
+++ LoginAuth/src/components/Profile/UserProfile.js
@@ -1,11 +1,11 @@
 import ProfileForm from './ProfileForm';
 import classes from './UserProfile.module.css';
 
-const UserProfile = () => {
+const UserProfile = ({ token, logout }) => {
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm token={token} logout={logout} />
     </section>
   );
 };
