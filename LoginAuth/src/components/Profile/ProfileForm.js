676a028abec81be9653255b2e5223fd8ed8383e3
João Pedro <pedrobarros2010@gmail.com>
2021-05-28 15:58:49 -0300

    feat: create password redefinition funciton

--- LoginAuth/src/components/Profile/ProfileForm.js
+++ LoginAuth/src/components/Profile/ProfileForm.js
@@ -1,17 +1,45 @@
+import { useRef } from 'react';
 import classes from './ProfileForm.module.css';
+import firebaseRequest from '../../services/firebaseRequest';
 
-const ProfileForm = () => {
+const ProfileForm = ({ token, logout }) => {
+  const newPasswordRef = useRef();
+  const request = firebaseRequest(
+    'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCb8jUUCUIIJHY_HMEuomEPDaPgrWSGNOs'
+  );
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const newPasswordValue = newPasswordRef.current.value;
+
+    await request(
+      JSON.stringify({
+        idToken: token,
+        password: newPasswordValue,
+        returnSecureToken: false,
+      })
+    );
+
+    alert(`You'll have to login again now!`);
+    logout();
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          minLength="6"
+          ref={newPasswordRef}
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
