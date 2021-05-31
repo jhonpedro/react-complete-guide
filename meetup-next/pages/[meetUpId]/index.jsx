e4f8a7881307e1471f955cdea05a464ae2ea528c
João Pedro <pedrobarros2010@gmail.com>
2021-05-31 14:23:50 -0300

    feat: first basic dummy meetup detail page

--- meetup-next/pages/[meetUpId]/index.jsx
+++ meetup-next/pages/[meetUpId]/index.jsx
@@ -1,7 +1,15 @@
 import { useRouter } from 'next/router';
+import MeetupDetail from '../../components/meetups/MeetupDetail';
 
 export default function MeetupSpecification() {
   const { meetUpId } = useRouter().query;
 
-  return <h1>Especific MeetUp: {meetUpId}</h1>;
+  return (
+    <MeetupDetail
+      image="http://s.glbimg.com/jo/g1/f/original/2015/01/30/candelaria-alexandre-macieira.jpg"
+      title="Dummy"
+      address="Dummy"
+      description="Dummyyy"
+    />
+  );
 }
