1417a6d50036ff08b8dad406fc8b382361a11c93
João Pedro <pedrobarros2010@gmail.com>
2021-05-31 11:51:49 -0300

    feat: create dummy meetups and add meetup list component

---
+++ meetup-next/pages/index.jsx
@@ -0,0 +1,40 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const initial_value = [
+  {
+    id: 1,
+    title: 'First Meetup',
+    image:
+      'http://s.glbimg.com/jo/g1/f/original/2015/01/30/candelaria-alexandre-macieira.jpg',
+    adress: 'Rua dos bobos, 0',
+    description: `Let's go to the church`,
+  },
+  {
+    id: 2,
+    title: 'Second Meetup',
+    image:
+      'http://s.glbimg.com/jo/g1/f/original/2015/01/30/candelaria-alexandre-macieira.jpg',
+    adress: 'Rua dos bobos, 0',
+    description: `Let's go again to the church`,
+  },
+  {
+    id: 3,
+    title: 'Third Meetup',
+    image:
+      'http://s.glbimg.com/jo/g1/f/original/2015/01/30/candelaria-alexandre-macieira.jpg',
+    adress: 'Rua dos bobos, 0',
+    description: `Let's go again and again to the church`,
+  },
+  {
+    id: 4,
+    title: 'Forth Meetup',
+    image:
+      'http://s.glbimg.com/jo/g1/f/original/2015/01/30/candelaria-alexandre-macieira.jpg',
+    adress: 'Rua dos bobos, 0',
+    description: `Let's go again and again and again to the church`,
+  },
+];
+
+export default function MainPage() {
+  return <MeetupList meetups={initial_value} />;
+}
