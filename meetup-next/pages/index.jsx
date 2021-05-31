03482fb263ffbd911ca846caad20c7022b90f1a5
João Pedro <pedrobarros2010@gmail.com>
2021-05-31 14:39:03 -0300

    feat: use context instead of static variable

--- meetup-next/pages/index.jsx
+++ meetup-next/pages/index.jsx
@@ -1,40 +1,9 @@
 import MeetupList from '../components/meetups/MeetupList';
-
-const initial_value = [
-  {
-    id: 1,
-    title: 'First Meetup',
-    image:
-      'http://s.glbimg.com/jo/g1/f/original/2015/01/30/candelaria-alexandre-macieira.jpg',
-    adress: 'Rua dos bobos, 0',
-    description: `Let's go to the church`,
-  },
-  {
-    id: 2,
-    title: 'Second Meetup',
-    image:
-      'http://s.glbimg.com/jo/g1/f/original/2015/01/30/candelaria-alexandre-macieira.jpg',
-    adress: 'Rua dos bobos, 0',
-    description: `Let's go again to the church`,
-  },
-  {
-    id: 3,
-    title: 'Third Meetup',
-    image:
-      'http://s.glbimg.com/jo/g1/f/original/2015/01/30/candelaria-alexandre-macieira.jpg',
-    adress: 'Rua dos bobos, 0',
-    description: `Let's go again and again to the church`,
-  },
-  {
-    id: 4,
-    title: 'Forth Meetup',
-    image:
-      'http://s.glbimg.com/jo/g1/f/original/2015/01/30/candelaria-alexandre-macieira.jpg',
-    adress: 'Rua dos bobos, 0',
-    description: `Let's go again and again and again to the church`,
-  },
-];
+import { useMeetups } from '../store/meetups';
 
 export default function MainPage() {
-  return <MeetupList meetups={initial_value} />;
+  const { getMeetups } = useMeetups();
+  const meetupsList = getMeetups();
+
+  return <MeetupList meetups={meetupsList} />;
 }
