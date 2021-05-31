553fcea46c8d02e928339f2120dec797d5b589f5
João Pedro <pedrobarros2010@gmail.com>
2021-05-31 16:25:20 -0300

    feat: set meetups in app start

--- meetup-next/pages/_app.js
+++ meetup-next/pages/_app.js
@@ -1,15 +1,22 @@
 import '../styles/globals.css';
 import Layout from '../components/layout/Layout';
 import MeetupProvider from '../store/meetups';
+import initial_value from '../store/initial-value';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, meetups }) {
   return (
     <Layout>
-      <MeetupProvider>
+      <MeetupProvider meetups={meetups}>
         <Component {...pageProps} />
       </MeetupProvider>
     </Layout>
   );
 }
+
+MyApp.getInitialProps = async () => {
+  return {
+    meetups: initial_value,
+  };
+};
 
 export default MyApp;
