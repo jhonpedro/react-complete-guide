00a3ef907de283fa1ebdded73035a11231369156
João Pedro <pedrobarros2010@gmail.com>
2021-06-01 16:05:19 -0300

    feat: use CSSTransition classes to create the animation

--- animations/src/components/Modal/Modal.js
+++ animations/src/components/Modal/Modal.js
@@ -1,29 +1,24 @@
 import React from 'react';
-import { Transition } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 
 import './Modal.css';
 
 const modal = (props) => {
   return (
-    <Transition in={props.show} timeout={500} mountOnEnter unmountOnExit>
-      {(state) => {
-        const modalClass = `Modal ${
-          state === 'entering'
-            ? 'ModalShow'
-            : state === 'exiting'
-            ? 'ModalHide'
-            : null
-        }`;
-        return (
-          <div className={modalClass}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
-      }}
-    </Transition>
+    <CSSTransition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={1000}
+      classNames="fade-modal"
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
