627c0ae95830d9d847742d792991c8b6b48e0d29
João Pedro <pedrobarros2010@gmail.com>
2021-06-01 11:34:07 -0300

    feat: post new meetup on api endpoint

--- meetup-next/pages/newMeetup/index.jsx
+++ meetup-next/pages/newMeetup/index.jsx
@@ -1,8 +1,20 @@
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import { useMeetups } from '../../store/meetups';
 
 export default function NewMeetUp() {
-  const addNewMeetupHandler = (newMeetup) => {
-    console.log(newMeetup);
+  const { addMeetup } = useMeetups();
+
+  const addNewMeetupHandler = async (newMeetup) => {
+    const response = await fetch('http://localhost:3000/api/newMeetup', {
+      method: 'POST',
+      body: JSON.stringify(newMeetup),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+    addMeetup(data.data);
   };
 
   return <NewMeetupForm onAddMeetup={addNewMeetupHandler} />;
