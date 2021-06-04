c0c373786459b9e88319745b785784de4de9f9fa
João Pedro <pedrobarros2010@gmail.com>
2021-06-04 11:25:00 -0300

    fix: use todos context

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,29 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
-import Todo from './models/todo';
+import { useTodos } from './store/todos';
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([new Todo('Learn typescript')]);
-
-  const addTodo = (todoText: string) => {
-    setTodos((prevState) => {
-      console.log(prevState);
-      return [new Todo(todoText), ...prevState];
-    });
-  };
-
-  const removeTodoItem = (text: string) => {
-    setTodos((prevState) => {
-      return prevState.filter((todo) => todo.text !== text);
-    });
-  };
+  const { addTodo, removeTodo, todos } = useTodos();
 
   return (
     <div className="App">
       <NewTodo addTodo={addTodo} />
-      <Todos items={todos} removeTodoItem={removeTodoItem} />
+      <Todos items={todos} removeTodoItem={removeTodo} />
     </div>
   );
 }
--- typescript/src/index.tsx
+++ typescript/src/index.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { TodosContextProvider } from './store/todos';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <TodosContextProvider>
+    <App />
+  </TodosContextProvider>,
+  document.getElementById('root')
+);
