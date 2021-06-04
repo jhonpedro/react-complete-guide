6680ff68f4c448ba2d9069ae6341677f6e9cc016
João Pedro <pedrobarros2010@gmail.com>
2021-06-04 10:48:31 -0300

    feat: create state, addTodo function and use NewTodo component

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [new Todo('Learn typescript'), new Todo('Learn testing')];
+  const [todos, setTodos] = useState<Todo[]>([new Todo('Learn typescript')]);
+
+  const addTodo = (todoText: string) => {
+    setTodos((prevState) => {
+      console.log(prevState);
+      return [new Todo(todoText), ...prevState];
+    });
+  };
 
   return (
     <div className="App">
+      <NewTodo addTodo={addTodo} />
       <Todos items={todos} />
     </div>
   );
